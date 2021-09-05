f22e62b5bea703a0a30ca051ec787d3d927b30cd
nndeto <86444679+nndeto@users.noreply.github.com>
2021-09-05 13:26:21 -0400

    got images to display on show page and added some comments

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,10 +1,14 @@
 function CityList(props) {
-    /* console.log(props.oneCity) */
+   
+    //updates props, which triggers calling it on our city container page
     function handleCityClick(idx) {
         props.updateCityIndex(idx)
         console.log("list element clicked" + idx)
     }
 
+    //variable that loops through the city data and returns just a name for our "sidebar"
+        //what's most important here is that it has an onClick function that will get our index back to our container 
+        //to update that state
     const allCities = props.cityData.map((oneCity, idx) => {
         return (
             <li
@@ -19,11 +23,7 @@ function CityList(props) {
         <div>
             {allCities}
         </div>
-
-
-
     )
-
 }
 
 export default CityList
