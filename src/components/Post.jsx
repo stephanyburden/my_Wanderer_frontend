072ca3a2aed19ce787e576e9ef9204ed4067c2a9
nndeto <86444679+nndeto@users.noreply.github.com>
2021-09-08 18:08:36 -0400

    finished cityshow

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -5,7 +5,6 @@ function Post(props) {
     return(
         <div>
             <Link to = {`${props.cityId}/posts/${props.onePost._id}`}>
-                <h2 className = "post-caption">Adventures</h2>
                 <h3 className = "post-title">{props.onePost.title}</h3>
             </Link>
             
