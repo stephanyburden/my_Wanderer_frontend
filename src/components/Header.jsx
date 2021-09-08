0dd7ed21a148e2261ea584853e8d0cf215770fcb
npkane75 <neeraj.p.kane@gmail.com>
2021-09-08 16:13:45 -0700

    nav bar updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,16 @@
 import {Link} from 'react-router-dom'
 import '../css/Header.css'
-
 function Header(){
     return(
         <header>
             <nav className="page-header">
                 <ul>
-                    <li><Link to='/'><button className="btn nav-btn"><span>Home</span></button></Link></li>
-                    <li><Link to = '/about'><button className="btn nav-btn"><span>About</span></button></Link></li>
-                    <li><Link to='/cities'><button className="btn nav-btn"><span>All Cities</span></button></Link></li>
+                    <li><Link to='/'><button className="nav-btn" id="btn"><span id="text">Home</span></button></Link></li>
+                    <li><Link to = '/about'><button className="nav-btn" id="btn"><span id="text">About</span></button></Link></li>
+                    <li><Link to='/cities'><button className="nav-btn" id="btn"><span id="text">All Cities</span></button></Link></li>
                 </ul>
             </nav>
         </header>
     )
 }
 export default Header
-
