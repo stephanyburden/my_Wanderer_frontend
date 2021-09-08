926a19ab255f676d6f3746a671009ac18c83d2fc
cotsao <codytsao@gmail.com>
2021-09-08 09:58:42 -0700

    post show route

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,10 +3,10 @@ import {Link} from 'react-router-dom'
 function Post(props) {
     return(
         <div>
-            <Link to = {`cities/${props.cityId}/posts/${props.onePost._id}`}>
+            <Link to = {`${props.cityId}/posts/${props.onePost._id}`}>
                 <h3>{props.onePost.title}</h3>
             </Link>
-            <p>{props.onePost.content}</p>
+            
         </div>
     )
 }
