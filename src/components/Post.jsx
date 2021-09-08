971cb1a860028a75cb0a224d94b40597e02ac390
nndeto <86444679+nndeto@users.noreply.github.com>
2021-09-08 17:04:50 -0400

    changes

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,10 +1,12 @@
 import {Link} from 'react-router-dom'
+import '../css/CityShowPage.css';
 
 function Post(props) {
     return(
         <div>
             <Link to = {`${props.cityId}/posts/${props.onePost._id}`}>
-                <h3>{props.onePost.title}</h3>
+                <h2 className = "post-caption">Adventures</h2>
+                <h3 className = "post-title">{props.onePost.title}</h3>
             </Link>
             
         </div>
