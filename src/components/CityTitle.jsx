a136b40281db92bd48475fcca85d97db596b744a
cotsao <codytsao@gmail.com>
2021-09-08 13:36:46 -0700

    title above cities

---
+++ src/components/CityTitle.jsx
@@ -0,0 +1,11 @@
+import '../css/CityTitle.css'
+function CityTitle(){
+    return(
+        <div className="title-wrapper">
+            <div className="title-container">
+                <h3 clasName="title-text">Popular Locations</h3>
+            </div>
+        </div>
+    )
+}
+export default CityTitle
